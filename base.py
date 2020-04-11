b61fb82028b4eee0ced3909bc749fc73afdcdb9a
Sebastian Michoń <sebastian.michon10@protonmail.com>
2020-04-11 19:05:27 +0200

    Index skeleton

--- base.py
+++ base.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect, request
 
 app=Flask(__name__)
 
@@ -6,9 +6,24 @@ app=Flask(__name__)
 def EuclidAlgo():
     return render_template('Gcd.html')
 
-@app.route('/')
+@app.route('/erasto')
 def ErastotenesSieve():
     return render_template('Primes.html')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def Wisdom():
+    req=request
+    if (req.method=='POST'):
+        s=req.form['next']
+        if (s=='sieve'):
+            return redirect(url_for('ErastotenesSieve'))
+        if (s=='gcd'):
+            return redirect(url_for('EuclidAlgo'))
+
+    else:
+        return render_template('index.html')
+    
 
 @app.context_processor
 def jinjautils():
