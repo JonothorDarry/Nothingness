9c617a3484ffeb77fab5665edbda8b985a76bea3
Sebastian Michoń <sebastian.michon10@protonmail.com>
2020-04-16 03:53:11 +0200

    Better utils, more precise theses

--- base.py
+++ base.py
@@ -2,29 +2,52 @@ from flask import Flask, render_template, url_for, redirect, request
 
 app=Flask(__name__)
 
-@app.route('/gcd')
+
+def comeBackin(place):
+    if (place=='sieve'):
+        return redirect(url_for('ErastotenesSieve'))
+    if (place=='gcd'):
+        return redirect(url_for('EuclidAlgo'))
+    if (place=='crt'):
+        return redirect(url_for('ChineseTheorem'))
+    if (place=='index'):
+        return render_template('index.html')
+
+
+@app.route('/gcd', methods=['GET', 'POST'])
 def EuclidAlgo():
-    return render_template('Gcd.html')
+    req=request
+    if (req.method=='POST'):
+        s=req.form['next']
+        return comeBackin(s)
+    else:
+        return render_template('Gcd.html')
 
-@app.route('/erasto')
+@app.route('/erasto', methods=['GET', 'POST'])
 def ErastotenesSieve():
-    return render_template('Primes.html')
+    req=request
+    if (req.method=='POST'):
+        s=req.form['next']
+        return comeBackin(s)
+    else:
+        return render_template('Primes.html')
 
-@app.route('/crt')
+@app.route('/crt', methods=['GET', 'POST'])
 def ChineseTheorem():
-    return render_template('Crt.html')
+    req=request
+    if (req.method=='POST'):
+        s=req.form['next']
+        return comeBackin(s)
+
+    else:
+        return render_template('Crt.html')
 
 @app.route('/', methods=['GET', 'POST'])
 def Wisdom():
     req=request
     if (req.method=='POST'):
         s=req.form['next']
-        if (s=='sieve'):
-            return redirect(url_for('ErastotenesSieve'))
-        if (s=='gcd'):
-            return redirect(url_for('EuclidAlgo'))
-        if (s=='crt'):
-            return redirect(url_for('ChineseTheorem'))
+        return comeBackin(s)
 
     else:
         return render_template('index.html')
