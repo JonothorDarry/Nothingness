56641a4149a4247ddb3ee29e2a961fae25915162
Sebastian Michoń <sebastian.michon10@protonmail.com>
2020-05-10 01:29:22 +0200

    Better binary expo anime

--- static/BinaryExpo.js
+++ static/BinaryExpo.js
@@ -2,8 +2,10 @@ var alldict={};
 
 class BinaryExpo extends Algorithm{
 	BeginningExecutor(){
-		this.btlist=[]
-		this.lees=[]
+		this.btlist=[];
+		this.utilbts=[];
+
+		this.lees=[];
 		this.place.innerHTML='';
 		var fas=this.input.value;
 		var a=0, b=0, x, i=0, c, m;
@@ -27,43 +29,68 @@ class BinaryExpo extends Algorithm{
 		var i=0, j=0, z=Math.floor(Math.log2(b))+1, btn, crb=b, mylst=[];
 		for (i=0;i<z;i++) mylst.push(crb%2), crb=Math.floor(crb/2);
 
+		this.divCreator();
 		for (j=0;j<3;j++){
 			for (i=0;i<z;i++) {
 				if (j==1 && i==z-1) btn=this.buttCreator(a, '#004400');
-				else if (j==2) btn=this.buttCreator(mylst[z-i-1], i==(z-1)?'#004400':'#440000'), crb/=2;
+				else if (j==2) btn=this.buttCreator(mylst[z-i-1], i==(z-1)?'#004400':'#440000');
 				else btn=this.buttCreator();
 				this.btlist[j].push(btn);
-				console.log(i==z-1, j==1, a, z);
-				this.place.appendChild(btn);
+				
+				this.zdivs[j][2].appendChild(btn);
 			}
-			this.place.appendChild(document.createElement("BR"))
+			if (j==1) btn=this.buttCreator(a);
+			if (j==2) btn=this.buttCreator(b);
+			if (j==0) btn=this.buttCreator(1);
+			
+			this.utilbts.push(btn);
+			this.zdivs[j][1].appendChild(btn);
 		}
 		this.reality=z-1;
 	}
 
+	Painter(btn, col=1){
+		if (col==0 || col==1) btn.style.color="#FFFFFF";
+		else btn.style.backgroundColor="#FFFFFF";
+
+		if (col==0) btn.style.backgroundColor="#440000";
+		else if (col==1) btn.style.backgroundColor="#004400";
+		else if (col==2) btn.style.color="#666666";
+		else if (col==3) btn.style.color="#FFFFFF";
+	}
+
 	StateMaker(){
 		var l=this.lees.length;
-		var s=this.lees[l-1], col;
+		var s=this.lees[l-1], col, vl;
 
 		if (s[0]>=100) return;
 		var a=s[1], b=s[2], res=s[3], m=s[4], i=0;
 
 		if (s[0]==0){
-			this.btlist[0][this.reality].style.backgroundColor='#004400';
-			this.btlist[0][this.reality].innerHTML=(res*(b%2==0?1:a))%m;
+			this.Painter(this.btlist[0][this.reality], 1);
+			vl=(res*(b%2==0?1:a))%m;
+			this.btlist[0][this.reality].innerHTML=vl;
+			this.utilbts[0].innerHTML=vl;			
 		}
 
 		else if (s[0]==1){
-			this.btlist[0][this.reality].style.backgroundColor='#440000';
+			this.Painter(this.btlist[0][this.reality], 2);
+			this.Painter(this.btlist[1][this.reality], 2);
+			this.Painter(this.btlist[2][this.reality], 2);
 
-			this.btlist[1][this.reality].style.backgroundColor='#440000';
-			this.btlist[2][this.reality].style.backgroundColor='#440000';
-			this.btlist[1][this.reality-1].style.backgroundColor='#004400';
-			this.btlist[2][this.reality-1].style.backgroundColor='#004400';
+			this.Painter(this.btlist[1][this.reality-1], 1);
+			this.Painter(this.btlist[2][this.reality-1], 1);
+
 			this.btlist[1][this.reality-1].innerHTML=(a*a)%m;
+			this.utilbts[1].innerHTML=(a*a)%m;	
+			this.utilbts[2].innerHTML=Math.floor(b/2);			
 			this.reality--;
 		}
 	}
+
+
+
+	StatementComprehension(){}
 
 	NextState(){
 		var l=this.lees.length;
@@ -75,15 +102,38 @@ class BinaryExpo extends Algorithm{
 		if (s[0]==0 && b==1) this.lees.push([100]);
 		else if (s[0]==0) this.lees.push([1, a, b, (res*(b%2==0?1:a))%m, m]);
 		else if (s[0]==1) this.lees.push([0, (a*a)%m, Math.floor(b/2), res, m]); 
-
 	}
 
+	divCreator(){
+		var divs=[], zdivs=[], i, j;
+		for (i=0;i<3;i++) divs.push(document.createElement("DIV")), zdivs.push([]);
+		for (i=0;i<3;i++){
+			divs[i].style.width="100%";
+			divs[i].style.height="40px";
+			for (j=0;j<3;j++) {
+				zdivs[i].push(document.createElement("DIV"));
+				zdivs[i][j].style.margin="0";
+				zdivs[i][j].style.padding="0";
+				zdivs[i][j].style.display="inline flow-root";
+				divs[i].appendChild(zdivs[i][j]);
+			}
+			if (i==0) zdivs[i][0].innerHTML="Current result:";
+			if (i==1) zdivs[i][0].innerHTML="Current a:";
+			if (i==2) zdivs[i][0].innerHTML="Current b:";
+			zdivs[i][0].style.width="200px";
+			zdivs[i][1].style.width="50px";
+
+			this.place.appendChild(divs[i]);
+		}
+		this.divs=divs;
+		this.zdivs=zdivs;
+	}
 	
 	//Creates buttons
 	buttCreator(numb=null, col='#440000'){
 		var butt=document.createElement("BUTTON");
-		butt.style.width="30px";
-		butt.style.height="30px";
+		butt.style.width="40px";
+		butt.style.height="40px";
 		butt.style.backgroundColor=col;
 		butt.style.border="0";
 		butt.style.padding='0';
