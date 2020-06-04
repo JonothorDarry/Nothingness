c2b1b10adda284a1e1bbe502d8cdc98dafc25c8e
Sebastian Michoń <sebastian.michon10@protonmail.com>
2020-06-04 21:32:30 +0200

    Diameter anime

--- static/TreeBasics.js
+++ static/TreeBasics.js
@@ -11,35 +11,43 @@ class Tree extends Algorithm{
 	treeConstructor(){
 		var n, i=0, j=0, a, b, width=this.place.offsetWidth, floater, angle, cval;
 		var edges=[[1, 2], [3, 4], [2, 4], [3, 5], [3, 6], [3, 7]];
-		var tr=[], widvs=[];
+		var widvs=[];	
+		this.tr=[];
+		this.width=width/100;
 
 		edges=this.getTreeFromInput();
 		n=this.n;
 
-		for (i=0;i<=n;i++) tr.push([]), widvs.push(0);
+		this.butts=[];
+		this.divis=[];
+		this.buttData=[];
+		for (i=0;i<=n;i++) this.tr.push([]), this.butts.push(0), this.divis.push(0), this.buttData.push(0), widvs.push(0);
 
 		for (i=0;i<edges.length;i++){
 			a=edges[i][0];
 			b=edges[i][1];
 
-			tr[a].push(b), tr[b].push(a);
+			this.tr[a].push(b), this.tr[b].push(a);
 		}
 
-		var params=this.dfsen(n, tr)
+		var params=this.dfsen(n, this.tr)
 		var depth=params[1];
 		var par=params[0];
-
 
 		this.place.style.position="relative";
 		for (i=0;i<depth.length;i++){
 			for (j=0;j<depth[i].length;j++){
 				a=depth[i][j];
 				var bt=this.buttCreator(a);
-				var dv=document.createElement("DIV");
+				this.butts[a]=bt;
+
 				floater=(j/depth[i].length+1/(2*depth[i].length));
 				widvs[a]=width*floater;
 
 				if (a!=1){
+					var dv=document.createElement("DIV");
+					this.divis[a]=dv;
+
 					dv.style.position="absolute";
 					cval=Math.sqrt(Math.pow(widvs[a]-widvs[par[a]], 2)+75*75);
 					dv.style.width=`${cval}px`;
@@ -64,6 +72,7 @@ class Tree extends Algorithm{
 				bt.style.position="absolute";
 				bt.style.top=`${i*75}px`;
 				bt.style.left=`${100*floater}%`;
+				this.buttData[a]={'left': 100*floater, 'top':i*75}
 
 				this.place.appendChild(bt);
 			}
@@ -103,6 +112,7 @@ class Tree extends Algorithm{
 				s.push(b);
 			}
 		}
+		this.par=par
 		return [par, sysdep];
 	}
 
@@ -149,8 +159,111 @@ class Tree extends Algorithm{
 		}
 		return butt;
 	}
+	
+	//0: red, 1:green, 2: gray, 3: dead white
+	Painter(btn, col=1){
+		if (col==0 || col==1 || col==5) btn.style.color="#FFFFFF";
+		else btn.style.backgroundColor="#FFFFFF";
+
+		if (col==0) btn.style.backgroundColor="#440000";
+		else if (col==1) btn.style.backgroundColor="#004400";
+		else if (col==2) btn.style.color="#666666";
+		else if (col==3) btn.style.color="#FFFFFF";
+		else if (col==5) btn.style.backgroundColor="#000000"
+	}
 }
 
+class DiamFinder extends Tree{
+	BeginningExecutor(){
+		super.BeginningExecutor();
+		var dimension, valar, valar2, x, y;
+
+		this.lees.push([0, 1])
+		this.ij=[0]
+		this.dp=[0]
+		this.dpval=[0]
+
+		for (var i=1;i<=this.n;i++){
+			this.ij.push(0)
+			y=this.buttData[i].top-20/Math.sqrt(2);
+			x=this.buttData[i].left+40/(Math.sqrt(2)*this.width);
+
+			valar=this.betterButtCreator(0);
+			valar2=this.betterButtCreator(0);
+			valar.style.top=`${y}px`;
+			valar.style.left=`${x}%`;
+
+			valar2.style.top=`${y}px`;
+			valar2.style.left=`${x+20/(Math.sqrt(2)*this.width)}%`;
+
+			this.dp.push([valar, valar2]);
+			this.dpval.push([0, 0]);
+			this.place.appendChild(valar);
+			this.place.appendChild(valar2);
+		}
+	}
+
+	StateMaker(){
+		var l=this.lees.length;
+		var s=this.lees[l-1], a, v0, para;
+					
+		if (s[0]==100) return;
+
+		a=s[1];
+		v0=this.ij[a];
+		if (s[0]==0 || s[0]==1){
+			this.Painter(this.butts[a], 1);
+			if (a!=1) this.Painter(this.butts[this.par[a]], 5);
+		}
+
+		if (s[0]==2){
+
+		}
+
+		if (s[0]==3){
+			this.Painter(this.butts[a], 2);
+			if (a!=1) {
+				para=this.par[a];
+				this.Painter(this.butts[para], 1);
+				if (this.dpval[para][0]<this.dpval[a][0]+1)		this.dpval[para][1]=this.dpval[para][0], this.dpval[para][0]=this.dpval[a][0]+1;
+				else if (this.dpval[para][1]<this.dpval[a][0]+1)	this.dpval[para][1]=this.dpval[a][0]+1;
+
+				this.dp[para][0].innerHTML=this.dpval[para][0];
+				this.dp[para][1].innerHTML=this.dpval[para][1];
+
+				this.butts[a].style.border="1px solid";
+				this.butts[a].style.borderColor="#888888";
+			}
+		}
+
+	}
+
+
+	NextState(){
+		var l=this.lees.length;
+		var s=this.lees[l-1], a, v0;
+		a=s[1];
+		if (s[0]==3) a=this.par[a];
+		v0=this.ij[a];
+
+		if (v0>=this.tr[a].length && a==1) this.lees.push([100]);
+		else if (v0>=this.tr[a].length) this.lees.push([3, a]);
+		else if (this.tr[a][v0]==this.par[a]) this.lees.push([2, a]);
+		else this.lees.push([1, this.tr[a][v0]]);
+		this.ij[a]+=1;
+	}
+
+
+	betterButtCreator(numb=null, col='#440000'){
+		var butt=this.buttCreator(numb, col);
+		butt.style.width="20px";
+		butt.style.height="20px";
+		butt.style.borderRadius="0%";
+		butt.style.position="absolute";
+
+		return butt;
+	}
+}
 
 var feral=Algorithm.ObjectParser(document.getElementById('Algo1'));
-var eg1=new Tree(feral);
+var eg1=new DiamFinder(feral);
