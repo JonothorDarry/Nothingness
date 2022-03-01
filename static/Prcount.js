aebec50095c88a3474d061f7a17df37c3addb08e
Sebastian Michoń <sebastian.michon10@protonmail.com>
2022-03-02 00:09:35 +0100

    Small fix

--- static/Prcount.js
+++ static/Prcount.js
@@ -740,4 +740,4 @@ var feral1=Algorithm.ObjectParser(document.getElementById('Algo1'));
 var sk1=new PostPhi(feral1, 121, 7);
 
 var feral2=Algorithm.ObjectParser(document.getElementById('Algo2'));
-var sk2=new Segtree_Counter(feral2, 3, [{'interval':7, 'prime_nr':2}, {'interval':15, 'prime_nr':3}, {'interval':11, 'prime_nr':0}]);
+var sk2=new Segtree_Counter(feral2, 5, [{'interval':7, 'prime_nr':2}, {'interval':15, 'prime_nr':3}, {'interval':11, 'prime_nr':0}, {'interval':14, 'prime_nr':4}, {'interval':12, 'prime_nr':3}]);
