5114b12d25bace6c98dbfdd92c540e40f4f8e164
Sebastian Michoń <sebastian.michon10@protonmail.com>
2022-03-09 01:08:19 +0100

    Totient IEP interpretation, part I

--- static/Totient.js
+++ static/Totient.js
@@ -15,13 +15,12 @@ class Totient_CRT extends Partial{
 
 				if (this.logic.table[i][j] == 0) grid.single_filler([i, j], `-`, {'color':32});
 				else grid.single_filler([i, j], this.logic.table[i][j], {'color':color});
-
 			}
 		}
 
-		grid.single_filler([-1, -1], 'n\\m', {'color':101});
-		this.buttons.upper = grid.filler([-1, [0, this.logic.m-1]], ArrayUtils.range(0, this.logic.m-1), {'color':5});
-		this.buttons.lefty = grid.filler([[0, this.logic.n-1], -1], ArrayUtils.range(0, this.logic.n-1), {'color':5});
+		grid.single_filler([-1, -1], 'n\\m', {'color':5});
+		this.buttons.upper = grid.filler([-1, [0, this.logic.m-1]], ArrayUtils.range(0, this.logic.m-1), {'color':101});
+		this.buttons.lefty = grid.filler([[0, this.logic.n-1], -1], ArrayUtils.range(0, this.logic.n-1), {'color':101});
 		var phi_n = this.logic.n, phi_m = this.logic.m;
 		for (i=0; i<this.logic.m; i++){
 			if (NTMath.gcd(i, this.logic.m) > 1) Representation_utils.Painter(this.buttons.upper[i], 6), phi_m--;
@@ -33,9 +32,9 @@ class Totient_CRT extends Partial{
 		var _size=90;
 		table.divs[this.logic.n+2].style.display = "none";
 		table.divs[this.logic.n+3].style.display = "none";
-		grid.single_filler([this.logic.n, -1], `&phi;(${this.logic.n}) = ${phi_n}`, {'color':101, 'stylistic':{'general':{'writing-mode':'vertical-rl'}, 'px':{'height':_size}}});
-		grid.single_filler([-1, this.logic.m], `&phi;(${this.logic.m}) = ${phi_m}`, {'color':101, 'stylistic':{'px':{'width':_size}}});
-		grid.single_filler([this.logic.n, this.logic.m], `&phi;(${this.logic.n * this.logic.m}) = ${phi_nm}`, {'color':101, 'stylistic':{'px':{'height':_size, 'width':_size}}});
+		grid.single_filler([this.logic.n, -1], `&phi;(${this.logic.n}) = ${phi_n}`, {'color':8, 'stylistic':{'general':{'writing-mode':'vertical-rl'}, 'px':{'height':_size}}});
+		grid.single_filler([-1, this.logic.m], `&phi;(${this.logic.m}) = ${phi_m}`, {'color':8, 'stylistic':{'px':{'width':_size}}});
+		grid.single_filler([this.logic.n, this.logic.m], `&phi;(${this.logic.n * this.logic.m}) = ${phi_nm}`, {'color':8, 'stylistic':{'px':{'height':_size, 'width':_size}}});
 	}
 
 	read_data(){
@@ -52,7 +51,6 @@ class Totient_CRT extends Partial{
 			this.logic.redundant[i%this.logic.n][i%this.logic.m]++;
 			this.logic.table[i%this.logic.n][i%this.logic.m] = i;
 		}
-		console.log(this.logic.redundant);
 	}
 
 	ShowReality(){
@@ -66,6 +64,146 @@ class Totient_CRT extends Partial{
 		super(block);
 		this.logic.n = n;
 		this.logic.n = m;
+		this.ShowReality();
+	}
+}
+
+class Totient_IEP extends Partial{
+
+	_logical_mark_numbers(){
+		var i=0, j, x=this.logic.n;
+		for (i=2; i*i<=x; i++){
+			if (x % i == 0) this.logic.prime_factors.push(i);
+			while (x % i == 0) x = Math.floor(x/i);
+		}
+		if (x>1) this.logic.prime_factors.push(x);
+		for (i=0; i<this.logic.prime_factors.length; i++) this.logic.inverse_factors[this.logic.prime_factors[i]] = i;
+
+		this.logic.prime_divs = ArrayUtils.steady(this.logic.n+1, 0).map(e => []);
+		for (i=1; i<=this.logic.n; i++){
+			for (j=0; j<this.logic.prime_factors.length; j++){
+				if (i % this.logic.prime_factors[j] == 0) this.logic.prime_divs[i].push(this.logic.prime_factors[j]);
+			}
+		}
+	}
+
+	_logical_generate_power_sets(){
+		this.logic.power_sets = [{'set':[], 'amount':this.logic.n, 'parity':1}];
+
+		var lim=0, x, i;
+		for (var p of this.logic.prime_factors){
+			lim = this.logic.power_sets.length;
+			for (i=0; i<lim; i++){
+				x = this.logic.power_sets[i];
+				this.logic.power_sets.push({'set':x.set.slice().concat(p), 'amount':Math.floor(x.amount/p), 'parity':-x.parity});
+			}
+		}
+	}
+
+	logical_box(){
+		this.logic.prime_factors = [];
+		this.logic.inverse_factors = {};
+
+		this._logical_mark_numbers();
+		this._logical_generate_power_sets();
+	}
+
+	_presentation_fill_div_with_prime_factors(div, factors){
+		var colors = [30, 32, 33, 101, 15];
+		var buttons = [];
+
+		for (var i=0; i<factors.length; i++){
+			var prime = factors[i];
+
+			var button_factor = Modern_representation.button_creator(prime, {'general':{'backgroundColor':Modern_representation.colors[colors[this.logic.inverse_factors[prime]]], 'color':'#FFFFFF', 'bottom':0, 'left':0, 'position':'relative', 'verticalAlign':'top'}, 'px':{'height':this.presentation.radius, 'width':this.presentation.radius}, '%':{'borderRadius':100}});
+			buttons.push(button_factor);
+
+			div.appendChild(button_factor);
+		}
+		return buttons;
+	}
+
+	_presentation_make_dubs(number){
+		var radius = Math.max(20, 10*Math.max(...this.logic.prime_factors).toString().length);
+		var width = Math.max(80, radius*this.logic.prime_factors.length), height_small=radius, height_huge=radius;
+		var height_full = height_small+height_huge;
+
+		this.presentation.radius = radius;
+		this.presentation.width = width;
+		this.presentation.height_small = height_small;
+
+		var div_double = Modern_representation.div_creator('', {'general':{'border':'1px solid grey'}, 'px':{'height':height_full, 'width':width}});
+		var div_upper = Modern_representation.div_creator('', {'general':{'position':'absolute'}, 'px': {'height':height_small, 'width':width, 'top':0}});
+		var div_lower = Modern_representation.div_creator('', {'general':{'position':'absolute'}, 'px':{'height':height_huge, 'width':width, 'bottom':0}});
+		var button_upper = Modern_representation.button_creator(number, {'general':{'backgroundColor':Modern_representation.colors[0], 'color':'#FFFFFF', 'top':0, 'position':'relative', 'verticalAlign':'top'}, 'px':{'height':height_huge, 'width':width}});
+
+		if (this.logic.prime_divs[number].length == 0) Modern_representation.button_modifier(div_lower, {'stylistic':{'general':{'backgroundColor':Modern_representation.colors[0]}}});
+		else this.Painter(button_upper, 2);
+
+		this._presentation_fill_div_with_prime_factors(div_lower, this.logic.prime_divs[number]);
+
+		div_upper.appendChild(button_upper);
+		div_double.appendChild(div_upper);
+		div_double.appendChild(div_lower);
+
+		return {'number':button_upper, 'full':div_double};
+	}
+
+	_presentation_sieve_like(){
+		var div_sievelike = Modern_representation.div_creator('', {'%':{'width':60}});
+		this.place.appendChild(div_sievelike);
+		this.buttons.sieve_like = [];
+
+		var i, btn;
+		for (i=1; i <= this.logic.n; i++){
+			btn = this._presentation_make_dubs(i);
+			this.buttons.sieve_like.push(btn);
+			div_sievelike.appendChild(btn.full);
+		}
+	}
+
+	_presentation_summarizer(){
+		var div_summary = Modern_representation.div_creator('', {'%':{'width':35}});
+		this.place.appendChild(div_summary);
+
+		var ln = this.logic.power_sets.length+4;
+		var table = Representation_utils.proto_divsCreator(1, ln, [], null, div_summary, this.stylistic);
+		var grid = new Grid(ln-1, 4, this.stylistic, {'place':table.zdivs, 'top_margin':1, 'left_margin':1, 'divs':true});
+		'&#8709;'
+
+		var div;
+		for (var i=0; i<this.logic.power_sets.length; i++){
+			div = grid.get(i, 0);
+			Modern_representation.button_modifier(div, {'stylistic':{'general':{'border':'1px solid grey'}, 'px':{'width':80}}});
+			var buttons = this._presentation_fill_div_with_prime_factors(div, ArrayUtils.revert(this.logic.power_sets[i].set));
+
+			for (var x of buttons){
+				Modern_representation.button_modifier(x, {'stylistic':{'general':{'verticalAlign':'baseline', 'left':'', 'float':'right'}, 'px':{'right':0}}});
+			}
+		}
+	}
+
+	presentation(){
+		this.buttons = {};
+		this._presentation_sieve_like();
+		this._presentation_summarizer();
+	}
+
+	read_data(){
+		var c=this.dissolve_input(this.input.value);
+		this.logic.n = c.get_next();
+	}
+
+	ShowReality(){
+		this.starter();
+		this.read_data();
+		this.logical_box();
+		this.presentation();
+	}
+
+	constructor(block, n){
+		super(block);
+		this.logic.n = n;
 		this.ShowReality();
 	}
 }
@@ -386,6 +524,9 @@ class PowerTower extends Algorithm{
 
 var feral3=Partial.ObjectParser(document.getElementById('Algo3'));
 var sk3=new Totient_CRT(feral3, 10, 17);
+
+var feral4=Partial.ObjectParser(document.getElementById('Algo4'));
+var sk4=new Totient_IEP(feral4, 130);
 
 var feral=Algorithm.ObjectParser(document.getElementById('Algo1'));
 var sk=new TotientSieve(feral, 30);
