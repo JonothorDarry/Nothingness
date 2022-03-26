13c81ce1af7ba346a64f67782c2b70a43bfd91ca
Sebastian Michoń <sebastian.michon10@protonmail.com>
2022-03-26 15:50:19 +0100

    Changed tree test case

--- static/TreeBasics.js
+++ static/TreeBasics.js
@@ -762,4 +762,4 @@ var feral=Algorithm.ObjectParser(document.getElementById('Algo1'));
 var eg1=new DiamFinder(feral, [[1, 2], [1, 3], [3, 4], [4, 5], [3, 6], [3, 7], [7, 8]]);
 
  var feral2=Algorithm.ObjectParser(document.getElementById('Algo2'));
- var eg2=new DoubleWalk(feral2, [[1, 2], [1, 3], [3, 4], [4, 5], [3, 6], [3, 7], [7, 8]], [2, 3, 7]);
+ var eg2=new DoubleWalk(feral2, [[1, 2], [2, 3], [3, 4], [4, 5], [2, 6], [6, 7]], [1, 4, 7]);
