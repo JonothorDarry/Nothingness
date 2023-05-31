fda7affc036f312d182ed48b28bb381fc1e5f925
Sebastian Michoń <sebastian.michon10@protonmail.com>
2023-06-01 01:17:16 +0200

    Shadows glid the archways

--- static/Trees/STL/Small_to_large.js
+++ static/Trees/STL/Small_to_large.js
@@ -43,9 +43,17 @@ class Small_to_large extends Algorithm{
 		return this._presentation.color_mapping[color%7];
 	}
 
-	presentation_append_companion(vertex, x, y, name, color=5, width_height=30){
-		var platz = this.buttons.present_tree.get_place_for_companion_button(vertex, x, y, {'width':width_height, 'height':width_height});
-		var btn = Modern_representation.button_creator(name, {'general':{'position':'absolute', 'left':platz.left, 'top':platz.top}, 'px':{'width':width_height, 'height':width_height, 'font-size':11}});
+	presentation_append_companion(vertex, x, y, name, color=5, width_height=30, set=false){
+		var platz = this.buttons.present_tree.get_place_for_companion_button(vertex, x, y, {'width':width_height, 'height':width_height}, true);
+		var modifier = 0;
+		if (set){
+			var c = (Math.sqrt(2)-1)*width_height/2;
+			modifier = c/Math.sqrt(2); //From Pete Goras's theorem
+		}
+		var lefty = `${platz.left.slice(0, platz.left.length-1)} - ${modifier}px)`;
+		var topy = `${platz.top.slice(0, platz.top.length-1)} + ${modifier}px)`;
+
+		var btn = Modern_representation.button_creator(name, {'general':{'position':'absolute', 'left':lefty, 'top':topy}, 'px':{'width':width_height, 'height':width_height, 'font-size':11}});
 		Modern_representation.Painter(btn, color);
 
 		this.buttons.div_tree.appendChild(btn);
@@ -74,7 +82,7 @@ class Small_to_large extends Algorithm{
 		var top_margin = this.presentation.set_margin;
 
 		for (var i=1; i<=this.logic.tree.n; i++){
-			var set = this.presentation_append_companion(i, 1, 1, '', 31, 2*this.presentation.set_radius);
+			var set = this.presentation_append_companion(i, 1, 1, '', 31, 2*this.presentation.set_radius, true);
 			Modern_representation.style(set, {'borderRadius':'100%', 'backgroundColor':'rgba(179, 0, 0, 0.5)', 'zIndex':1});
 			this.buttons.sets.push(set);
 			this.buttons.set_elements.push([null]);
@@ -110,10 +118,17 @@ class Small_to_large extends Algorithm{
 
 	_presentation_construct_tree(){
 		this._presentation.edge_height = 2;
-		var width = 400*this.logic.tree.get_width(), height = 240*this.logic.tree.get_height();
+		this._presentation.vertex_size = 50;
+
+		this.presentation.set_size = Math.ceil(Math.sqrt(Math.max(...this.logic.tree.colors)));
+		var side_length = (this.presentation.set_size/2)*30;
+		this.presentation.set_radius = side_length*Math.sqrt(2);
+		this.presentation.set_margin = side_length*(Math.sqrt(2)-1);
+
+		var width = (400+2*this.presentation.set_radius-60)*this.logic.tree.get_width(), height = (240+2*this.presentation.set_radius-60)*this.logic.tree.get_height();
 		var div_tree = Modern_representation.div_creator('', {'px':{'width':width, 'height':height}});
 		var present_tree = new Modern_tree_presenter(this.logic.tree, {'div':div_tree, 'width':width, 'height':height}, {
-			'vertex':{'width':50, 'height':50, 'radius':100},
+			'vertex':{'width':this._presentation.vertex_size, 'height':this._presentation.vertex_size, 'radius':100},
 			'edge':{'height':this._presentation.edge_height},
 			'nonsense':this.stylistic
 		});
@@ -124,15 +139,14 @@ class Small_to_large extends Algorithm{
 		this.buttons.vertexes = present_tree.buttons.vertexes;
 		for (var edge of this.buttons.edges.slice(2)) Modern_representation.style(edge, {'background':'rgba(0, 0, 0, 0.15)'});
 
-		this.presentation.set_size = Math.ceil(Math.sqrt(Math.max(...this.logic.tree.colors)));
-		var side_length = (this.presentation.set_size/2)*30;
-		this.presentation.set_radius = side_length*Math.sqrt(2);
-		this.presentation.set_margin = side_length*(Math.sqrt(2)-1);
-
 		this.presentation_add_color_base();
 		this.presentation_add_set();
+		Modern_representation.style(div_tree, {'position':'relative', 'top':`${this.presentation.set_radius-60}px`});
 
-		return div_tree;
+		//Maybe unnecessary, but eases conversions
+		var tree_wrapper = Modern_representation.div_creator('', {'px':{'width':width, 'height':height+this.presentation.set_radius}});
+		tree_wrapper.appendChild(div_tree);
+		return tree_wrapper;
 	}
 
 	presentation(){
@@ -204,7 +218,7 @@ class Small_to_large extends Algorithm{
 
 		if (s[0]==0){
 			for (var i=1; i<=this.logic.tree.n; i++){
-				staat.push([7, this.buttons.set_elements[i][this.logic.tree.colors[i]].color, {'background':this._presentation.color_mapping[this.logic.tree.colors[i]], 'color':'#FFFFFF'}]);
+				staat.push([7, this.buttons.set_elements[i][this.logic.tree.colors[i]].color, {'background':this.presentation_get_color(this.logic.tree.colors[i]), 'color':'#FFFFFF'}]);
 				staat.push([0, this.buttons.results[0], 1]);
 			}
 		}
@@ -234,8 +248,8 @@ class Small_to_large extends Algorithm{
 
 				for (var x of operation[0]) staat.push([7, this.buttons.set_elements[vertex][x].color, {'background':'rgba(255, 255, 255, 0)', 'color':'rgba(255, 255, 255, 0)'}]);
 				for (var x of operation[1]) staat.push([7, this.buttons.set_elements[parental][x].color, {'background':'rgba(255, 255, 255, 0)', 'color':'rgba(255, 255, 255, 0)'}]);
-				for (var x of operation[0]) staat.push([7, this.buttons.set_elements[parental][x].color, {'background':this._presentation.color_mapping[x], 'color':'#FFFFFF'}]);
-				for (var x of operation[1]) staat.push([7, this.buttons.set_elements[vertex][x].color, {'background':this._presentation.color_mapping[x], 'color':'#FFFFFF'}]);
+				for (var x of operation[0]) staat.push([7, this.buttons.set_elements[parental][x].color, {'background':this.presentation_get_color(x), 'color':'#FFFFFF'}]);
+				for (var x of operation[1]) staat.push([7, this.buttons.set_elements[vertex][x].color, {'background':this.presentation_get_color(x), 'color':'#FFFFFF'}]);
 			}
 			else {
 				staat.push([7, this.buttons.sets[vertex], {'background':this._presentation.colors_set.pointed}]);
@@ -250,7 +264,7 @@ class Small_to_large extends Algorithm{
 			var next_element = [...operation[+operation[2]]][index];
 
 			this.modern_pass_color(this.buttons.set_elements[vertex][next_element].overlay, 14, 104);
-			staat.push([7, this.buttons.set_elements[this.logic.tree.par[vertex]][next_element].color, {'background':this._presentation.color_mapping[next_element], 'color':'#FFFFFF'}]);
+			staat.push([7, this.buttons.set_elements[this.logic.tree.par[vertex]][next_element].color, {'background':this.presentation_get_color(next_element), 'color':'#FFFFFF'}]);
 			this.modern_pass_color(this.buttons.set_elements[this.logic.tree.par[vertex]][next_element].overlay, 1, 104);
 		}
 
@@ -323,6 +337,30 @@ class Small_to_large extends Algorithm{
 
 			return `Now, we merge connected components containing vertices ${vertex} and par(${vertex})=${this.logic.tree.par[vertex]}. Also, the result (for now) for f(${vertex}) is set to the previous result - f(${vertex}+1) = ${this.logic.subsequent_results[this.logic.n-vertex]} - the colors from already existing connected components are retained in the result, the question is, how many colors are present in both (just merged) connected components - this amount will be subtracted from the result.`;
 		}
+		if (s[0]==2){
+			var vertex = s[1];
+			var operation = this.logic.operation_per_vertex[vertex];
+			var primary = `Now, we check, which set is bigger, and, if the parent is associated with a smaller set, we swap them. `;
+			
+			var secondary;
+			if (operation[2]) secondary = `Guess what - the size of the set associated with a vertex ${vertex} (size equal to ${operation[0].size}) is bigger than the size of the set associated with its parent - par(${vertex}) = ${this.logic.tree.par[vertex]} (size equal to ${operation[1].size}). And so, we swap those sets. The crucial thing is to do it in O(1) - for example, swapping sets by reference.`;
+			else secondary = `The size of the set associated with a vertex ${vertex} (size equal to ${operation[0].size}) is smaller or equal to the size of the set associated with its parent - par(${vertex}) = ${this.logic.tree.par[vertex]} (size equal to ${operation[1].size}). And so, we don't swap those sets.`;
+			return primary + secondary;
+		}
+		
+		if (s[0]==3 || s[0]==4){
+			var vertex = s[1];
+			var index = s[2];
+			var operation = this.logic.operation_per_vertex[s[1]];
+			var next_operation = [...operation[+operation[2]]];
+
+			var start = `Next, it is checked, whether next element from the set associated with the vertex belongs to the set associated with its parent. `;
+			var ending;
+			if (s[0]==4) ending = `Clearly, ${next_operation[index]} does not belong to the set associated with the parent of this vertex. Thus, we add this element to the set associated with the parent of this vertex.`;
+			if (s[0]==4) ending = `Clearly, ${next_operation[index]} already belong to the set associated with the parent of this vertex. Thus, we subtract one from the current result.`;
+			return start+ending;
+		}
+		if (s[0] == 101) return `And so, all the sums of sizes of distinct sets after each merge of two connected components were found, and so the algorithm ends.`;
 	}
 }
 export default Small_to_large;
